6352c9f91043b1d96534bf11f6155083d91c3d6a
Nikita <asdezert@gmail.com>
2019-08-19 21:51:10 +0300

    Labor - 5

--- Lab5/src/ua/zp/brainacad/Lab4Test.java
+++ Lab5/src/ua/zp/brainacad/Lab4Test.java
@@ -7,23 +7,24 @@ public class Lab4Test {
 
         // PART 1
         // 1.1)
-        float[] firstArray; // TODO init it's array by new with size 4.
+        float[] firstArray = new float[4]; // init it's array by new with size 4.
 
-        // TODO insert some value to start of array and to end of array.
+        firstArray = new float[] {1, 2, 3, 4}; // insert some value to start of array and to end of array.
 
-        int[] intArray; // TODO init and fill it's array with any values. Use {} syntax.
+        int[] intArray = {5, 6, 7, 8}; // init and fill it's array with any values. Use {} syntax.
 
         // 1.2)
-        int[] intArrayCopy = {}; // TODO copy "intArray". Use copyOf...
+        int[] intArrayCopy = Arrays.copyOf(intArray, intArray.length); // copy "intArray". Use copyOf...
 
         // 1.3)
-        // TODO sort "intArrayCopy", use Arrays sort.
+        Arrays.sort(intArrayCopy); //sort "intArrayCopy", use Arrays sort.
 
         // 1.4)
-        // TODO print "intArrayCopy", use Arrays toString.
+        Arrays.toString(intArrayCopy); // print "intArrayCopy", use Arrays toString.
 
         // 1.5)
-        // TODO compare "intArray"  and "intArrayCopy", use Arrays equals. Print "Arrays equals" or "Arrays not equals".
+        System.out.println(Arrays.equals(intArray, intArrayCopy) ? "Массивы равны" : "Масивы неравны");
+        // compare "intArray"  and "intArrayCopy", use Arrays equals. Print "Arrays equals" or "Arrays not equals".
 
 
         // PART 2
@@ -35,14 +36,38 @@ public class Lab4Test {
         }
 
         // 2.1)
-        // TODO calc sum of all array elements and print result.
+        // calc sum of all array elements and print result.
+        int sum1 = 0;
+        for (int item1 : testArray) {
+            sum1 += item1;
+        }
+        System.out.println(" - Сумма: " + sum1);
+
 
         // 2.2)
-        // TODO calc sum of all numbers with odd indexes and print result.
+        // calc sum of all numbers with odd indexes and print result.
+        int sum2 = 0;
+        for (int item2 : testArray) {
+            if (item2%2 != 0) {
+                sum2 += item2;
+            }
+        }
+        System.out.println("Сумма нечетных: " + sum2);
+
 
         // 2.3)
         // TODO find max value in array.
+        int max = Integer.MIN_VALUE;
+        for (int item3: testArray) {
+            if (item3 > max) {
+                max = item3;
+            }
+        }
+        System.out.println("аксимальное значение: " + max);
 
+        //ДЛЯ СЕБЯ - просортировал и вывел массив
+        Arrays.sort(testArray);
+        System.out.println("Сортировка: " + Arrays.toString(testArray));
 
         // PART 3
 
